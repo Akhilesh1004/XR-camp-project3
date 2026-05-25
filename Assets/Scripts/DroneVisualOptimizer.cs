b8698b706d840ce7a0b12e308da9de102bb21e45
Akhilesh1004 <akhileshcaelestisabyss@gmail.com>
2026-05-25 20:47:00 +0800

    update main

---
+++ Assets/Scripts/DroneVisualOptimizer.cs
@@ -0,0 +1,116 @@
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class DroneVisualOptimizer
+{
+    private Renderer[] renderers;
+    private Animator[] animators;
+    private MeshCollider[] meshColliders;
+    private bool initialized;
+    private bool visible = true;
+
+    public void Initialize(
+        GameObject owner,
+        bool disableChildMeshColliders,
+        bool optimizeRendererSettings
+    )
+    {
+        if (owner == null)
+        {
+            return;
+        }
+
+        renderers = owner.GetComponentsInChildren<Renderer>(true);
+        animators = owner.GetComponentsInChildren<Animator>(true);
+        meshColliders = owner.GetComponentsInChildren<MeshCollider>(true);
+
+        if (disableChildMeshColliders && meshColliders != null)
+        {
+            for (int i = 0; i < meshColliders.Length; i++)
+            {
+                MeshCollider meshCollider = meshColliders[i];
+
+                if (meshCollider != null)
+                {
+                    meshCollider.enabled = false;
+                }
+            }
+        }
+
+        if (optimizeRendererSettings && renderers != null)
+        {
+            for (int i = 0; i < renderers.Length; i++)
+            {
+                Renderer renderer = renderers[i];
+
+                if (renderer == null)
+                {
+                    continue;
+                }
+
+                renderer.shadowCastingMode = ShadowCastingMode.Off;
+                renderer.receiveShadows = false;
+                renderer.motionVectorGenerationMode = MotionVectorGenerationMode.ForceNoMotion;
+                renderer.lightProbeUsage = LightProbeUsage.Off;
+                renderer.reflectionProbeUsage = ReflectionProbeUsage.Off;
+            }
+        }
+
+        if (animators != null)
+        {
+            for (int i = 0; i < animators.Length; i++)
+            {
+                Animator animator = animators[i];
+
+                if (animator != null)
+                {
+                    animator.cullingMode = AnimatorCullingMode.CullUpdateTransforms;
+                }
+            }
+        }
+
+        initialized = true;
+    }
+
+    public void SetVisible(bool shouldBeVisible, bool disableAnimatorsWhenHidden)
+    {
+        if (!initialized || visible == shouldBeVisible)
+        {
+            return;
+        }
+
+        visible = shouldBeVisible;
+
+        if (renderers != null)
+        {
+            for (int i = 0; i < renderers.Length; i++)
+            {
+                Renderer renderer = renderers[i];
+
+                if (renderer != null)
+                {
+                    renderer.enabled = shouldBeVisible;
+                }
+            }
+        }
+
+        if (disableAnimatorsWhenHidden && animators != null)
+        {
+            for (int i = 0; i < animators.Length; i++)
+            {
+                Animator animator = animators[i];
+
+                if (animator != null)
+                {
+                    animator.enabled = shouldBeVisible;
+                }
+            }
+        }
+    }
+
+    public void ForceVisible(bool disableAnimatorsWhenHidden)
+    {
+        visible = false;
+        SetVisible(true, disableAnimatorsWhenHidden);
+    }
+}
